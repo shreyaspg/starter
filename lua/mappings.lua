0ec11d795c490dfe14683f84b62d3e93a59c0b09
shreyaspg <shreyasgaikwad19@gmail.com>
2024-03-24 11:29:24 +0530

    first changes
    Author:    shreyaspg <shreyasgaikwad197@gmail.com>
    Date:      Sun Mar 24 11:29:24 2024 +0530

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,7 +4,46 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
+-- map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- quick writes and close
+map("n", "<leader>w", ":w<CR>", { desc = "quick write" })
+map("n", "<leader>q", ":q<CR>", { desc = "quick exit" })
+
+-- move block  +/-
+map("v", "J", ":m '>+1<CR>gv=gv", { desc = "move block down" })
+map("v", "K", ":m '<-2<CR>gv=gv", { desc = "move block up" })
+
+-- use jk to escape
+map("t", "jk", "<C-\\><C-n>", { desc = "move block down" })
+
+-- scroll and center
+map("n", "<C-d>", "<C-d>zz", { desc = "scroll and center" })
+
+-- nvimtree
+map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Nvimtree Tree Toggle" })
+
+-- tabufline
+map("n", "<S-l>", function()
+  require("nvchad.tabufline").next()
+end, { desc = "Buffer Goto next" })
+
+map("n", "<S-h>", function()
+  require("nvchad.tabufline").prev()
+end, { desc = "Buffer Goto prev" })
+
+-- use alt 1-9 to switch tabs
+for i = 1, 9, 1 do
+  vim.keymap.set("n", string.format("<A-%s>", i), function()
+    vim.api.nvim_set_current_buf(vim.t.bufs[i])
+  end)
+end
+
+-- Disable mappings
+local nomap = vim.keymap.del
+
+nomap("n", "<C-n>")
+
+nomap("n", "<tab>")
+nomap("n", "<S-tab>")
